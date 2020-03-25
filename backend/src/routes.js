d9003df74707fd6c17f2972275cbb3f142ef085d
endersonlg <endersonluciog@gmail.com>
2020-03-24 22:53:40 -0300

    Rota de Get,Post de ongs e incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const routes = express.Router().
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
-  routes.get('/', (request, response) => {
-    return response.json({
-      evento: 'Semana Omnistack 11.0',
-      aluno: 'Enderson Garcia'
-    });
-  });
+const routes = express.Router();
 
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
 module.exports = routes;
